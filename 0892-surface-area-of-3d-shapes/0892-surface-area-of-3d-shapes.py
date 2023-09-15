0b9f3c15ec0ade3baaca307d9c93017286fb177e
Jisu Park <41144168+jisu98@users.noreply.github.com>
2023-09-15 21:12:33 +0900

    Time: 69 ms (98.77%), Space: 16.3 MB (70.37%) - LeetHub

--- 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
+++ 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
@@ -1,35 +1,19 @@
 class Solution:
     def surfaceArea(self, grid: List[List[int]]) -> int:
         result = 0
-        lenX, lenY = len(grid), len(grid[0])
         
-        for x in range(lenX):
-            for y in range(lenY):
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
                 cube = grid[x][y]
 
                 if cube == 0:
                     continue
                 else:
-                    result += 2 # top & bottom
+                    result += cube * 4 + 2
                     
-                if x == 0:
-                    result += cube
-                else:
-                    result += max(0, cube - grid[x - 1][y])
-                    
-                if y == 0:
-                    result += cube
-                else:
-                    result += max(0, cube - grid[x][y - 1])
-                    
-                if x == lenX - 1:
-                    result += cube
-                else:
-                    result += max(0, cube - grid[x + 1][y])
-                    
-                if y == lenY - 1:
-                    result += cube
-                else:
-                    result += max(0, cube - grid[x][y + 1])
+                if x > 0:
+                    result -= 2 * min(grid[x - 1][y], cube)
+                if y > 0:
+                    result -= 2 * min(grid[x][y - 1], cube)
                 
         return result
