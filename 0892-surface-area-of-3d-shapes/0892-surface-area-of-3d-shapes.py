560ff5f7c289850f1f8dfe3420f23b0b846009c5
Jisu Park <41144168+jisu98@users.noreply.github.com>
2023-09-15 21:15:45 +0900

    Time: 80 ms (81.17%), Space: 16.1 MB (100.00%) - LeetHub

--- 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
+++ 0892-surface-area-of-3d-shapes/0892-surface-area-of-3d-shapes.py
@@ -4,14 +4,16 @@ class Solution:
         
         for x in range(len(grid)):
             for y in range(len(grid[0])):
-                if grid[x][y] == 0:
+                cube = grid[x][y]
+
+                if cube == 0:
                     continue
-                
-                result += grid[x][y] * 4 + 2
+                else:
+                    result += cube * 4 + 2
                     
                 if x > 0:
-                    result -= 2 * min(grid[x - 1][y], grid[x][y])
+                    result -= 2 * min(grid[x - 1][y], cube)
                 if y > 0:
-                    result -= 2 * min(grid[x][y - 1], grid[x][y])
+                    result -= 2 * min(grid[x][y - 1], cube)
                 
         return result
