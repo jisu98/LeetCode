78a371d0babb7b9380554ad9dd5f9b8a031126b6
Jisu Park <41144168+jisu98@users.noreply.github.com>
2024-12-26 17:18:53 +0900

    Time: 103 ms (59.34%), Space: 42.5 MB (26.37%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.kt
@@ -0,0 +1,30 @@
+class Solution {
+    fun findTargetSumWays(nums: IntArray, target: Int): Int {
+        val numbers = nums.sorted().toList()
+        var results = mapOf<Int, Int>()
+
+        numbers.forEach { number ->
+            if (results.isEmpty()) {
+                results = if (number == 0) {
+                    mapOf(0 to 2)
+                } else {
+                    mapOf(number to 1, -number to 1)
+                }
+            } else {
+                val newResults = mutableMapOf<Int, Int>()
+
+                results.forEach { prevResult ->
+                    val sum = prevResult.key + number
+                    val difference = prevResult.key - number
+
+                    newResults[sum] = (newResults[sum] ?: 0) + prevResult.value
+                    newResults[difference] = (newResults[difference] ?: 0) + prevResult.value
+                }
+
+                results = newResults
+            }
+        }
+
+        return results[target] ?: 0
+    }
+}
