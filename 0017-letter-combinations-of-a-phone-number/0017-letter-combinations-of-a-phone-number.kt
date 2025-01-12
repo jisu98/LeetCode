627e175003aaf4265f94576b4f9ae7769017865b
Jisu Park <41144168+jisu98@users.noreply.github.com>
2025-01-12 23:51:22 +0900

    Time: 17 ms (42.14%), Space: 39.2 MB (27.23%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.kt
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.kt
@@ -2,23 +2,25 @@ class Solution {
     fun letterCombinations(digits: String): List<String> {
         if (digits.isEmpty()) return emptyList()
 
-        return letterCombinations(digits, listOf(""))
-    }
+        var result = listOf("")
 
-    private fun letterCombinations(digits: String, list: List<String>): List<String> {
-        if (digits.isEmpty()) return list
-
-        return letterCombinations(digits.drop(1), list.flatMap { s ->
-            when (digits.first()) {
-                '2' -> listOf("${s}a", "${s}b", "${s}c")
-                '3' -> listOf("${s}d", "${s}e", "${s}f")
-                '4' -> listOf("${s}g", "${s}h", "${s}i")
-                '5' -> listOf("${s}j", "${s}k", "${s}l")
-                '6' -> listOf("${s}m", "${s}n", "${s}o")
-                '7' -> listOf("${s}p", "${s}q", "${s}r", "${s}s")
-                '8' -> listOf("${s}t", "${s}u", "${s}v")
-                else -> listOf("${s}w", "${s}x", "${s}y", "${s}z")
+        digits.forEach { digit ->
+            result = result.flatMap { string ->
+                when (digit) {
+                    '2' -> listOf('a', 'b', 'c')
+                    '3' -> listOf('d', 'e', 'f')
+                    '4' -> listOf('g', 'h', 'i')
+                    '5' -> listOf('j', 'k', 'l')
+                    '6' -> listOf('m', 'n', 'o')
+                    '7' -> listOf('p', 'q', 'r', 's')
+                    '8' -> listOf('t', 'u', 'v')
+                    else -> listOf('w', 'x', 'y', 'z')
+                }.map {
+                    string + it
+                }
             }
-        })
+        }
+        
+        return result
     }
 }
