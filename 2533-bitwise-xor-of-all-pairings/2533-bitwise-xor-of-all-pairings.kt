a334f853c89f05834f8e069a904a68544182a22a
Jisu Park <41144168+jisu98@users.noreply.github.com>
2025-01-16 16:58:29 +0900

    Time: 22 ms (50%), Space: 57.5 MB (0%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.kt
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.kt
@@ -1,8 +1,6 @@
 class Solution {
     fun xorAllNums(nums1: IntArray, nums2: IntArray): Int {
-        val a = if (nums1.size % 2 == 0) 0 else nums2.reduce { x, y -> x.xor(y) }
-        val b = if (nums2.size % 2 == 0) 0 else nums1.reduce { x, y -> x.xor(y) }
-
-        return a.xor(b)
+        return (if (nums1.size % 2 == 0) 0 else nums2.reduce(Int::xor)) xor
+                (if (nums2.size % 2 == 0) 0 else nums1.reduce(Int::xor))
     }
 }
