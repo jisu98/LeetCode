337a2ec0897667c548444082924f09c096a42065
Jisu Park <41144168+jisu98@users.noreply.github.com>
2025-04-01 20:54:10 +0900

    Time: 28 ms (12.5%), Space: 104.8 MB (93.75%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.kt
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.kt
@@ -1,20 +1,15 @@
 class Solution {
     fun mostPoints(questions: Array<IntArray>): Long {
         val len = questions.size
-        val solvePoints = LongArray(len) { 0L }
-        val skipPoints = LongArray(len) { 0L }
+        val points = LongArray(len) { 0L }
 
-        solvePoints[len - 1] = questions.last()[0].toLong()
-
-        ((len - 2) downTo 0).forEach { index ->
-            skipPoints[index] = maxOf(solvePoints[index + 1], skipPoints[index + 1])
-            solvePoints[index] = maxOf(
-                questions[index][0] +
-                        (solvePoints.getOrNull(index + questions[index][1] + 1) ?: 0L),
-                solvePoints[index + 1],
+        ((len - 1) downTo 0).forEach { index ->
+            points[index] = maxOf(
+                points.getOrNull(index + 1) ?: 0L,
+                questions[index][0] + (points.getOrNull(index + questions[index][1] + 1) ?: 0L),
             )
         }
 
-        return maxOf(skipPoints[0], solvePoints[0])
+        return points[0]
     }
 }
