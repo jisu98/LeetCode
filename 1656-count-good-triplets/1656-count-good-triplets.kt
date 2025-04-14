66510b0b01bc30e3db1efd8d107a5c85ca6232b9
Jisu Park <41144168+jisu98@users.noreply.github.com>
2025-04-14 13:52:18 +0900

    Time: 150 ms (89.47%), Space: 43.4 MB (10.53%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.kt
+++ 1656-count-good-triplets/1656-count-good-triplets.kt
@@ -2,9 +2,9 @@ class Solution {
     fun countGoodTriplets(arr: IntArray, a: Int, b: Int, c: Int): Int {
         return (0..arr.size - 3).sumOf { i ->
             (i + 1..arr.size - 2).sumOf { j ->
-                (j + 1..arr.size - 1).count { k ->
-                    abs(arr[i] - arr[j]) <= a &&
-                            abs(arr[k] - arr[j]) <= b &&
+                if (abs(arr[i] - arr[j]) > a) 0
+                else (j + 1..arr.size - 1).count { k ->
+                    abs(arr[k] - arr[j]) <= b &&
                             abs(arr[i] - arr[k]) <= c
                 }
             }
