aa194817f599aa3f327f4d7580af6f002fe73c9a
Jisu Park <41144168+jisu98@users.noreply.github.com>
2025-08-27 20:11:07 +0900

    Time: 27 ms (68.52%), Space: 52.9 MB (50%) - LeetHub

--- 0561-array-partition/0561-array-partition.kt
+++ 0561-array-partition/0561-array-partition.kt
@@ -1,7 +1,13 @@
 class Solution {
     fun arrayPairSum(nums: IntArray): Int {
-        return nums.sorted()
-        .filterIndexed { i, _ -> i % 2 == 0 }
-        .sum()
+        var answer = 0
+
+        nums.sort()
+
+        for (i in nums.indices step 2) {
+            answer += nums[i]
+        }
+
+        return answer
     }
 }
