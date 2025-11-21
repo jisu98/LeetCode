f5266bd4c04f5c3ef6db58554b232f573c2b2f0f
Jisu Park <41144168+jisu98@users.noreply.github.com>
2025-11-21 17:13:18 +0900

    Time: 31 ms (100%), Space: 53.2 MB (28.57%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.kt
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.kt
@@ -1,29 +1,33 @@
 class Solution {
     fun countPalindromicSubsequence(s: String): Int {
-        val len = s.count()
-        val palindromeSet = mutableSetOf<Pair<Char, Char>>()
-        val notPalindromeSet = mutableSetOf<Pair<Char, Char>>()
+        val alphabetToIndex = Array(26) { mutableListOf<Int>() }
+        var count = 0
 
-        for (i in 0..<(len - 2)) {
-            if (palindromeSet.any { it.first == s[i] }) continue
-            if (notPalindromeSet.any { it.first == s[i] }) continue
+        for (i in s.indices) {
+            alphabetToIndex[s[i] - 'a'] += i
+        }
 
-            for (j in (i + 1)..<(len - 1)) {
-                if (Pair(s[i], s[j]) in palindromeSet) continue
-                if (Pair(s[i], s[j]) in notPalindromeSet) continue
+        for (i in 0 until 26) {
+            if (alphabetToIndex[i].size < 2) continue
 
-                for (k in j + 1..<len) {
-                    if (s[i] == s[k]) {
-                        palindromeSet += Pair(s[i], s[j])
-                    }
-                }
-                
-                if (Pair(s[i], s[j]) !in palindromeSet) {
-                    notPalindromeSet += Pair(s[i], s[j])
-                }
+            for (j in 0 until 26) {
+                if (hasElementInRange(
+                        alphabetToIndex[j],
+                        alphabetToIndex[i].first(),
+                        alphabetToIndex[i].last(),
+                    )
+                ) count++
             }
         }
 
-        return palindromeSet.size
+        return count
+    }
+
+    private fun hasElementInRange(list: List<Int>, start: Int, end: Int): Boolean {
+        val index = list.binarySearch(start + 1).let {
+            if (it >= 0) it else -it - 1
+        }
+
+        return index < list.size && list[index] < end
     }
 }
