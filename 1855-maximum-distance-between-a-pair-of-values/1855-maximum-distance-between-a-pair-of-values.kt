b8fd1c04584e254363bebe1a16ba383aac9946bd
Jisu Park <41144168+jisu98@users.noreply.github.com>
2026-04-19 23:56:06 +0900

    Time: 5 ms (100%), Space: 62.7 MB (81.82%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.kt
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.kt
@@ -1,28 +1,19 @@
 class Solution {
     fun maxDistance(nums1: IntArray, nums2: IntArray): Int {
         var distance = 0
+        var i = 0
+        var j = 0
 
-        for (i in nums1.indices) {
-            distance = maxOf(distance, findLastLargerIndex(nums2, nums1[i]) - i)
-        }
-
-        return distance
-    }
-
-    private fun findLastLargerIndex(arr: IntArray, target: Int): Int {
-        var l = 0
-        var r = arr.size - 1
-        
-        while (l <= r) {
-            val mid = (l + r) / 2
-
-            if (arr[mid] >= target) {
-                l = mid + 1
+        while (i < nums1.size && j < nums2.size) {
+            if (nums1[i] <= nums2[j]) {
+                distance = maxOf(distance, j - i)
+                j++
             } else {
-                r = mid - 1
+                i++
+                if (j < i) j = i
             }
         }
 
-        return l - 1
+        return distance
     }
 }
