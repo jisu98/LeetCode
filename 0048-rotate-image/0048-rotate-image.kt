3164424c4ae3c20306f13eb44786ae1b7ca1d17a
Jisu Park <41144168+jisu98@users.noreply.github.com>
2026-05-04 10:19:23 +0900

    Time: 56 ms (5%), Space: 49.9 MB (6.67%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun rotate(matrix: Array<IntArray>): Unit {
+        val n = matrix.size
+
+        for (x in 0 until n / 2) {
+            for(y in 0 until (n + 1) / 2) {
+                val tmp = matrix[x][y]
+
+                matrix[x][y] = matrix[n - y - 1][x]
+                matrix[n - y - 1][x] = matrix[n - x - 1][n - y - 1]
+                matrix[n - x - 1][n - y - 1] = matrix[y][n - x - 1]
+                matrix[y][n - x - 1] = tmp
+
+                println("$x $y")
+                for (i in 0 until n) println(matrix[i].toList())
+            }
+        }
+    }
+}
